f495c7ae51804da67c63cd783a6a3d9ebad54d6a
AyoubRavash <ayoubravash@gmail.com>
2026-06-19 16:29:21 +0330

    debug: fix image height

--- src/components/Projects/Project/Project.tsx
+++ src/components/Projects/Project/Project.tsx
@@ -2,7 +2,7 @@ import type {ProjectModel} from './Project.d'
 
 function Project({image, title, technologies, description}: ProjectModel) {
     return <div className={'border border-gray'}>
-        {image && (<img className={'h-7/12 w-full'} src={image} alt="Project Image"/>)}
+        {image && (<img className={'h-1/2 w-full'} src={image} alt="Project Image"/>)}
         <ul className={'border border-gray border-r-0 border-l-0 flex items-center gap-2 text-gray p-2'}>
             {technologies.map(t => <li key={t}>{t}</li>)}
         </ul>
