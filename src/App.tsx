fbaeabc6994fd31573353c5c089a2bba1cb53b3f
AyoubRavash <ayoubravash@gmail.com>
2026-06-19 14:25:42 +0330

    feature: adding just empty app pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import {Route, Routes} from "react-router-dom";
 import MainLayout from "./layouts/MainLayout.tsx";
 import HomePage from "./pages/HomePage.tsx";
 import AboutMePage from "./pages/AboutMePage.tsx";
+import WorksPage from "./pages/WorksPage.tsx";
+import ContactsPage from "./pages/ContactsPage.tsx";
 
 function App() {
     return <Routes>
         <Route path="/" element={<MainLayout/>}>
             <Route index element={<HomePage/>}/>
+            <Route path={'works'} element={<WorksPage/>}/>
             <Route path={'about-me'} element={<AboutMePage/>}/>
+            <Route path={'contacts'} element={<ContactsPage/>}/>
         </Route>
     </Routes>
 }
