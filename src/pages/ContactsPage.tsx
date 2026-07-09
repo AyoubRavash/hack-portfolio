430153ee9361a179b3f95d9fb7485db59cd62223
AyoubRavash <ayoubravash@gmail.com>
2026-07-09 16:56:13 +0330

    feature: adding content to contacts page

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -1,5 +1,14 @@
+import PageTitle from "../components/UI/PageTitle/PageTitle.tsx";
+import Contacts from "../components/Contacts/Contacts.tsx";
+import LargeSquare from "../components/UI/Squares/LargeSquare/LargeSquare.tsx";
+
 function ContactsPage() {
-    return <div></div>
+    return <div className={'flex flex-col gap-10'}>
+        <PageTitle title={'contacts'} description={'who am i?'}/>
+        <LargeSquare styles={'right-0 border-r-0 top-30'}/>
+        <Contacts/>
+        <LargeSquare styles={'left-0 border-l-0 top-100'}/>
+    </div>
 }
 
 export default ContactsPage;
